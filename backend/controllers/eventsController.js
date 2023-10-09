2cded53fe33a9763ebed67ac2bc813c953c8a445
François-Xavier Savary <fx.savary@orange.fr>
2023-10-09 15:12:41 +0200

    creation API dans eventsController pour récupération de tous les évènements

--- backend/controllers/eventsController.js
+++ backend/controllers/eventsController.js
@@ -48,4 +48,42 @@ async function createEvent(req, res) {
     
 };
 
-module.exports = { createEvent };
+// controller pour recupération de tous les évènements
+async function getAllEvents(req, res) {
+
+  // requête sql pour obtenir tous les évènements
+  const eventsQuery = 'SELECT * FROM evenements';
+
+  // execute la requête pour les évènements
+  myEventsConnection.query(eventsQuery, (err, eventsResults) => {
+    if (err) {
+      console.error('Erreur lors de la récupération des évènements: ', err);
+      res.status(500).json({ error: 'Erreur lors de la récupération des évènements'});
+      return;
+    };
+
+    // formate les résultats en un objet JSON contenant les évènements 
+    const events = eventsResults;
+    const formattedData = formatData(events);
+    // renvoie les résultats au format JSON en réponse
+    res.json({ events: formattedData});
+
+  });
+
+};
+
+function formatData(events) {
+  const formattedEvents = events.map(evenement => ({
+    title: evenement.titre,
+    date: evenement.date,
+    location: evenement.lieu,
+    image: {
+      source: evenement.image_source,
+      alt: evenement.image_alt
+    },
+  }));
+
+  return formattedEvents;
+}; 
+
+module.exports = { createEvent, getAllEvents };
--- backend/routes/events.js
+++ backend/routes/events.js
@@ -6,6 +6,10 @@ const multer = require('multer'); // importe multer
 // configure le dossier de destination pour les fichiers téléchargés
 const upload = multer({ dest: '../assets/events-covers' });
 
+// route pour la création d'évèvenement
 router.post('/create', upload.single('eventCoverImage'),eventsController.createEvent);
+
+// route pour la récupération de tous les évènements
+router.get('/', eventsController.getAllEvents)
 
 module.exports = router;
