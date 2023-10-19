0d43ffc55826060608224c2dde74b9e2681315ac
François-Xavier Savary <fx.savary@orange.fr>
2023-10-19 13:30:08 +0200

    Recupération et affichage des données OK dans ParticipantsList suppression du store userStore

--- frontend/src/views/ParticipantsList.vue
+++ frontend/src/views/ParticipantsList.vue
@@ -1,8 +1,34 @@
 <template>
-  
+  <div>
+    <h1>Participants List</h1>
+    <ul>
+      <li v-for="participant in participants" :key="participant.email">
+        <!-- Affiche les détails du participant -->
+        <p>Nom de l'entreprise: {{ participant.nom_entreprise }}</p>
+        <p>Nom du participant: {{ participant.nom }}</p>
+        <!-- Ajoutez d'autres propriétés que vous souhaitez afficher ici -->
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { fetchParticipantsData } from '@/services/api';
+
+const route = useRoute();
+const participants = ref([]);
+
+onMounted(async () => {
+  const eventSlug = route.params.eventSlug;
+  try {
+    const response = await fetchParticipantsData(eventSlug); // Utilisez votre fonction fetch ici
+    participants.value = response.participants;
+  } catch (error) {
+    console.error('Erreur lors du chargement des participants :', error);
+  }
+});
 
 </script>
 
