225c3f31c99c9f0dd3bfdf441c0e35c5566a6b7c
François-Xavier Savary <fx.savary@orange.fr>
2023-10-21 14:56:43 +0200

    ajout BDD table 'administrateurs' installation en backend de bcrypt et jsonwebtoken

--- Back-up BDD/myevents.sql
+++ Back-up BDD/myevents.sql
@@ -17,7 +17,9 @@ CREATE TABLE evenements (
     logo_client_alt VARCHAR(255),
     site_client VARCHAR(255),
     slug VARCHAR(255) NOT NULL,
-    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    administrateur_id INT,
+    FOREIGN KEY (administrateur_id) REFERENCES administrateurs(id)
 );
 
 CREATE TABLE participants (
@@ -36,4 +38,12 @@ CREATE TABLE participants_evenements (
     FOREIGN KEY (participant_id) REFERENCES myevents.participants (id),
     FOREIGN KEY (evenement_id) REFERENCES myevents.evenements (id),
     UNIQUE KEY uc_participant_event (participant_id, evenement_id)
+);
+
+CREATE TABLE administrateurs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    mot_de_passe VARCHAR(255) NOT NULL
 );
