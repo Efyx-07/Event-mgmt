752a22c3a9266018711ed98b3ef9a425b29db3aa
Efyx-07 <fx.savary@orange.fr>
2023-11-06 11:32:25 +0100

    Maj sass logoutConfirmationModal

--- frontend/src/sub-components/LogoutConfirmationModal.vue
+++ frontend/src/sub-components/LogoutConfirmationModal.vue
@@ -63,55 +63,72 @@
 <style lang="scss" scoped>
 
     @import '@/assets/sass/variables.scss';
-    .hiddenLogoutConfirmationModal {
-        visibility: hidden;
-        opacity: 0;
-    }
-    .logoutConfirmationModal_container {
-        background: rgba($darkColor, .25);
-        backdrop-filter: blur(2.5px);
-        position: fixed;
-        top: 0;
-        width: 25vw;
-        height: 100%;
-        transition: opacity .1s ease-in-out;
-        .logoutConfirmationModal {
-            background: $accentColorBackof4;
-            border: solid 1px $accentColorBackof3;
-            border-radius: $containerRadius;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 1rem;
-            padding: 2rem;
-            position: absolute;
-            bottom: 3rem;
-            left: 50%;
-            transform: translate(-50%, -50%);
+    @import '@/assets/sass/varMediaQueries.scss';
 
-            p {
-                margin: 0;
-                color: $lightColor;
-            }
-            .buttons_container {
+    @media screen and (min-width: $breakpointDesktop) {
+        .hiddenLogoutConfirmationModal {
+            visibility: hidden;
+            opacity: 0;
+        }
+        .logoutConfirmationModal_container {
+            background: rgba($darkColor, .25);
+            backdrop-filter: blur(2.5px);
+            position: fixed;
+            top: 0;
+            width: 25vw;
+            height: 100%;
+            transition: opacity .1s ease-in-out;
+            .logoutConfirmationModal {
+                width: 90%;
+                background: $accentColorBackof4;
+                border: solid 1px $accentColorBackof3;
+                border-radius: $containerRadius;
                 display: flex;
+                flex-direction: column;
+                align-items: center;
                 gap: 1rem;
+                padding: 1rem 2rem;
+                position: absolute;
+                bottom: 3rem;
+                left: 50%;
+                transform: translate(-50%, -50%);
 
-                button {
-                    width: 8rem;
-                    background: transparent;
-                    border: solid 1px $accentColorBackof3;
-                    padding: .5rem;
-                    cursor: pointer;
+                p {
+                    margin: 0;
+                    color: $lightColor;
+                }
+                .buttons_container {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 1rem;
 
-                    &:hover {
-                        border-color: $accentColorBackof2;
-                        background: $accentColorBackof2;
+                    button {
+                        width: 8rem;
+                        background: transparent;
+                        border: solid 1px $accentColorBackof3;
+                        padding: .5rem;
+                        cursor: pointer;
+
+                        &:hover {
+                            border-color: $accentColorBackof2;
+                            background: $accentColorBackof2;
+                        }
                     }
                 }
             }
         }
     }
-
     
+    @media screen and (min-width: 1440px) {
+        .logoutConfirmationModal_container {
+            .logoutConfirmationModal {
+                width: unset;
+                padding: 2rem;
+                .buttons_container {
+                    flex-direction: row;
+                }
+            }
+        }
+    }
+
 </style>
