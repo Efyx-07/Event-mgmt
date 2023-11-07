1a7420c1fba32826dd3345032060f75f21c912e7
François-Xavier Savary <fx.savary@orange.fr>
2023-11-07 16:42:03 +0100

    Ajout clé API pour locationMaps, ok après test

--- frontend/src/components/EventPageLocationMap.vue
+++ frontend/src/components/EventPageLocationMap.vue
@@ -1,12 +1,38 @@
 <template>
 
-    <iframe class="locationMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10494.127685897449!2d2.364546728166933!3d48.88619910106311!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66dd75390f999%3A0x7214db08363c60a1!2sVillette%2C%2075019%20Paris!5e0!3m2!1sfr!2sfr!4v1698937822877!5m2!1sfr!2sfr" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe 
+        class="locationMap" 
+        width="600"
+        height="450"
+        style="border:0"
+        loading="lazy"
+        allowfullscreen
+        referrerpolicy="no-referrer-when-downgrade"
+        :src="embedUrl">
+    </iframe>
+
 </template>
 
 <script setup>
 
+    import { computed } from 'vue';
+
     // recupère la props de selectedEvents en provenance de EventPage
     const { selectedEvent } = defineProps(['selectedEvent']);
+
+    // clé google maps (à remplacer par la clé du client)
+    const googleMapsEmbedApiKey = 'AIzaSyBZqDbBpqGQwhdR34PVpRViRe1oFcvj4Mk';
+
+    // récupère le locationUrl de l'API evenements et le reformate avec la clé API selon format googleMaps
+    const embedUrl = computed(() => {
+        if (selectedEvent.locationUrl) {
+            const urlWithApiKey = `https://www.google.com/maps/embed/v1/place?key=${googleMapsEmbedApiKey}&q=${encodeURIComponent(selectedEvent.location)}`;
+            return urlWithApiKey;
+            // si aucun n'est trouvé renvoie une chaine de caractères vide
+        } else {
+            return '';
+        }
+    });
 
 </script>
 
