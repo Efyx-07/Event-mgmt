5edb52af2cb1b0e8d5c12425e9bacbe89ffedfe5
Efyx-07 <fx.savary@orange.fr>
2023-11-08 16:13:36 +0100

    Maj server.js

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,6 @@ const path = require('path');
 // charge les variables d'environnement à partir du fichier .env
 require('dotenv').config();
 
-console.log('GOOGLE_MAPS_API_KEY:', process.env.GOOGLE_MAPS_API_KEY);
-console.log('Variables d\'environnement chargées :', process.env);
-
 const app = express(); // demarre le serveur express
 const PORT = process.env.PORT || 3000; // définit le port
 
@@ -26,7 +23,6 @@ app.use('/assets', express.static(path.join(__dirname, 'assets')));
 // endpoint pour l'accés à la clé API  Google Maps Embed stockée dans les variables d'environnement
 app.get('/maps', (req, res) => {
     const urlWithApiKey = `https://www.google.com/maps/embed/v1/place?key=${process.env.GOOGLE_MAPS_API_KEY}&q=${encodeURIComponent(req.query.location)}`;
-    console.log('URL est: ', urlWithApiKey)
     res.json({ embedUrl: urlWithApiKey });
 });
 
